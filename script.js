064121bdda0618b56cf2110270537d609e4fbd49
Terrance Cazy <terrancecazy@MacBook-Pro.lan>
2021-04-23 19:33:43 -0500

    add data to localstorage

--- script.js
+++ script.js
@@ -6,9 +6,20 @@ const movieSelect = document.getElementById('movie');
 
 let ticketPrice = +movieSelect.value;
 
+function setMovieData(movieIndex, moviePrice){
+  localStorage.setItem('selectedMovieIndex', movieIndex);
+  localStorage.setItem('selectedMoviePrice', moviePrice)
+}
+
+
 
 function updateSelectedCount(){
   const selectedSeats = document.querySelectorAll('.row .seat.selected');
+
+  const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex))
+
+
   const selectedSeatsCount = selectedSeats.length;
   count.innerText = selectedSeatsCount;
   total.innerText = selectedSeatsCount * ticketPrice;
@@ -16,6 +27,7 @@ function updateSelectedCount(){
 
 movieSelect.addEventListener('change', e => {
   ticketPrice = +e.target.value;
+  setMovieData(e.target.selectedIndex, e.target.value);
   updateSelectedCount();
 })
 
