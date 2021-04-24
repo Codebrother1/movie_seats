7fe00ea5479b2227d623ac04b5e5d73f7c9f62f6
Terrance Cazy <terrancecazy@MacBook-Pro.lan>
2021-04-23 19:49:04 -0500

    populate ui with local storage data

--- script.js
+++ script.js
@@ -4,6 +4,9 @@ const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
+populateUI() ;
+
+
 let ticketPrice = +movieSelect.value;
 
 function setMovieData(movieIndex, moviePrice){
@@ -25,6 +28,24 @@ function updateSelectedCount(){
   total.innerText = selectedSeatsCount * ticketPrice;
 }
 
+function populateUI(){
+  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+  if(selectedSeats !== null && selectedSeats.length > 0){
+    seats.forEach((seat, index) => {
+      if(selectedSeats.indexOf(index) > -1){
+        seat.classList.add('selected');
+      }
+    });
+  }
+  const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+
+  if(selectedMovieIndex !== null){
+    movieSelect.selectedIndex = selectedMovieIndex;
+  }
+}
+
+
+
 movieSelect.addEventListener('change', e => {
   ticketPrice = +e.target.value;
   setMovieData(e.target.selectedIndex, e.target.value);
@@ -40,3 +61,4 @@ container.addEventListener('click', (e) => {
   }
 })
 
+updateSelectedCount();
